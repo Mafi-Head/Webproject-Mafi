a64656c1ed76b91abf3c83ab72f22b42b67f9926
Mafi-Head <mafi32162@gmail.com>
2025-08-07 15:05:41 +0200

    Update Pes.py

--- Pes.py
+++ Pes.py
@@ -23,6 +23,39 @@ pes1.stekaj()
 pes2.predstav_sa()
 pes2.stekaj()
 
+# Rozsirenie: Pridaj vlastnosť vek a metódu oslav_narodeniny(), ktorá vek zvýši o 1.
+
+# Trieda Pes:
+
+# Definujeme triedu Pes – šablónu pre vytváranie psov.
+
+class Pes:
+    def __init__(self, meno, plemeno, vek): # __init__ na nastavenie vlastností objektu!!!
+        self.meno = meno
+        self.plemeno = plemeno
+        self.vek = vek
+
+    def stekaj(self):
+        print(f"{self.meno}: Hav-hav!")
+
+    def predstav_sa(self):
+        print(f"Ahoj! Som {self.meno} a som {self.plemeno}.")
+
+    def oslav_narodeniny(self):
+        self.vek += 1
+        print(f"{self.meno}: Ma narodeniny! Ma uz {self.vek} rokov.")
+
+
+# Použitie:
+
+pes1 = Pes("Rex", "nemecký ovčiak",7)
+
+pes1.predstav_sa()
+pes1.stekaj()
+
+pes1.oslav_narodeniny()
+pes1.predstav_sa()
+
 
 
 
