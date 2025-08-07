360e1d1077046fdf9e95a1a0ff56a98fd6a596a0
Mafi-Head <mafi32162@gmail.com>
2025-08-07 15:24:46 +0200

    Update Pes.py

--- Pes.py
+++ Pes.py
@@ -56,6 +56,50 @@ pes1.stekaj()
 pes1.oslav_narodeniny()
 pes1.predstav_sa()
 
+# Rozsirenie: Trieda Pes s pridanou metódou daj_labku(), ktorá vytlačí správu: „<meno psa>: Podávam labku!“
+
+# Trieda Pes:
+
+# Definujeme triedu Pes – šablónu pre vytváranie psov.
+
+class Pes:
+    def __init__(self, meno, plemeno, vek): # __init__ na nastavenie vlastností objektu!!!
+        self.meno = meno
+        self.plemeno = plemeno
+        self.vek = vek
+
+    def stekaj(self):
+        print(f"{self.meno}: Hav-hav!")
+
+    def predstav_sa(self):
+        print(f"Ahoj! Som {self.meno} a som {self.plemeno}.")
+
+    def oslav_narodeniny(self):
+        self.vek += 1
+        print(f"{self.meno}: Ma narodeniny! Ma uz {self.vek} rokov.")
+
+    def daj_labku(self):
+        print(f"{self.meno}: Podavam labku!")
+
+
+# Použitie:
+
+pes1 = Pes("Rex", "nemecký ovčiak",7)
+pes2 = Pes("Loki", "cinsky chocholaty pes", 6)
+
+pes1.predstav_sa()
+pes1.stekaj()
+pes1.oslav_narodeniny()
+pes1.daj_labku()
+
+pes2.predstav_sa()
+pes2.stekaj()
+pes2.oslav_narodeniny()
+pes2.daj_labku()
+
+
+
+
 
 
 
